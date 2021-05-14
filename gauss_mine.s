9b7dc8476569bc72f4c8533445cfa862eb8588a1
Oscar Dahlqvist <oscdah@student.chalmers.se>
2021-05-14 16:16:17 +0200

    first working version

---
+++ gauss_mine.s
@@ -0,0 +1,885 @@
+### Text segment
+		.text
+start:
+		la		$a0, matrix_4x4		# a0 = A (base address of matrix)
+		li		$a1, 4    		    # a1 = N (number of elements per row)
+									# <debug>
+		jal 	print_matrix	    # print matrix before elimination
+		nop							# </debug>
+		jal 	eliminate			# triangularize matrix!
+		nop							# <debug>
+		jal 	print_matrix		# print matrix after elimination
+		nop							# </debug>
+		jal 	exit
+
+exit:
+		li   	$v0, 10          	# specify exit system call
+      	syscall						# exit program
+
+################################################################################
+# eliminate - Triangularize matrix.
+#
+# Args:		$a0  - base address of matrix (A)
+#			$a1  - number of elements per row (N)
+eliminate:
+		# void eliminate (double **A, int N) { 
+		# 	int i, j, k; 
+		#	k = 0;
+		# 	do { 				//forDiag
+		#		for (j = k + 1; j < N; j++) { 		//forRightOf
+		#			A[k][j] = A[k][j] / A[k][k];
+		#		}
+		#
+		#		A[k][k] = 1.0f;
+		#
+		# 		for (i = k + 1; i < N; i++) {		//forRightOf2
+		# 			for (j = k + 1; j < N; j++) {	//forAbove
+		# 				A[i][j] = A[i][j] - A[i][k] * A[k][j];
+		# 			}
+		# 			A[i][k] = 0.0; 
+		# 		} 
+		#		k++
+		#	} while(k<N)
+
+		# If necessary, create stack frame, and save return address from ra
+		addiu	$sp, $sp, -4		# allocate stack frame
+		sw		$ra, 0($sp)			# done saving registers
+
+		########### PART 1 ##########	
+		# $s0 = int k
+		# $s1 = int j		
+		# $s2 = int i
+		
+		# $f1 = const float 1.0f
+		
+		# $s3 = float* adressof A[k][k]
+		# $f3 = float A[k][k]
+		
+		# $s4 = float* adressof A[i][k]
+		# $f4 = float A[i][k]
+		
+		# $s5 = float* adressof A[i][j]
+		# $f5 = float A[i][j]
+
+		l.s $f1, float1.0
+				
+			move $s0, $0 # k = 0	
+doWhile_k:	# for (k = 0; k < N; k++) 
+			# TODO use a2 (row num) and a3 (colum num) directly to not move arounds the vars
+			
+			move $a2, $s0
+			move $a3, $s0
+			jal getElem
+			move $s3, $v0 	# s2 = &A[k][k]
+			mov.s $f3, $f0 	# f1 = A[k][k]
+			
+				add $s1,$s0,1 # j = k+1
+				b for_j_CMP
+for_j:			# for (j = k+1; j < N; j++) 
+				move $a2, $s0
+				move $a3, $s1
+				jal getElem
+				# f0 = A[k][j]
+				# v0 = adressof A[k][j]
+				
+				div.s $f0, $f0, $f3 # $f0 <- A[k][j] / A[k][k]
+				s.s $f0,0($v0)		# A[k][j] <- f0
+
+for_j_ITER:		add $s1,$s1,1		# j++
+for_j_CMP:		slt $t0,$s1,$a1		# t0=1 if $s1 < $a1 (j<N)
+				beq $t0,1,for_j		# if j < N
+				
+			s.s $f1, 0($s3) 	# A[k][k] <- 1.0f
+			
+				add $s2,$s0,1 # i = k+1
+				b for_i_CMP
+for_i:			# for (i = k+1; i < N; i++) 
+				move $a2, $s2 	# i -> a2
+				move $a3, $s0 	# k -> a3
+				jal getElem
+				move $s4, $v0 	# s4 = adressof A[i][k]
+				mov.s $f4, $f0 	# f4 = A[i][k]
+				
+					add $s1,$s0,1 # j = k+1
+					b for_j2_CMP
+for_j2:				# for (j = k + 1; j < N; j++)
+					move $a2, $s2 	# i -> a2 (already set the first time but not subsequent)
+					move $a3, $s1 	# j -> a3 
+					jal getElem
+					move $s5, $v0 	# s5 = adressof A[i][j]
+					mov.s $f5, $f0 	# f5 = A[i][j]
+					
+					move $a2, $s0 	# k -> a2
+					#move $a3, $s1	# j -> a3 (Redundant since A[i][j] and A[k][j] has the same column index
+					jal getElem
+					# f0 = A[k][j]
+					# v0 = adressof A[k][j]
+					
+					mul.s $f0, $f0, $f4 # f0 <- A[k][j] * A[i][k]
+					sub.s $f0, $f5, $f0 # f0 <- A[i][j] - f0
+					s.s $f0, 0($s5)		# f0 -> A[i][j]
+					
+for_j2_ITER:		add $s1,$s1,1		# j++
+for_j2_CMP:			slt $t0,$s1,$a1		# t0=1 if $s1 < $a1 (j<N)
+					beq $t0,1,for_j2	# if i < N
+					
+				sw $0, 0($s4) # direcly set A[i][k] to 0.0f (works since 0x0 = 0.0f)
+				
+for_i_ITER:		add $s2,$s2,1		# i++
+for_i_CMP:		slt $t0,$s2,$a1		# t0=1 if $s2 < $a1 (i<N)
+				beq $t0,1,for_i		# if i < N
+			
+			#
+
+			add $s0,$s0,1		# k++
+doWhile_k_CMP:	
+			slt $t0,$s0,$a1		# t0=1 if $s0 < $a1 (k<N)
+			beq $t0,1,doWhile_k		# if k < N
+		
+		##
+		##
+
+		lw		$ra, 0($sp)			# done restoring registers
+		addiu	$sp, $sp, 4			# remove stack frame
+
+		jr		$ra					# return from subroutine
+		nop							# this is the delay slot associated with all types of jumps
+
+################################################################################
+# getElem - Get address and content of matrix element A[a][b].
+#
+# Argument registers $a0..$a3 are preserved across calls
+#
+# Args:		$a0  - base address of matrix (A)
+#			$a1  - number of elements per row (N)
+#			$a2  - row number (a)
+#			$a3  - column number (b)
+#						
+# Returns:	$v0  - Address to A[a][b]
+#			$f0  - Contents of A[a][b] (single precision)
+getElem:
+		addiu	$sp, $sp, -12		# allocate stack frame
+		sw		$s2, 8($sp)
+		sw		$s1, 4($sp)
+		sw		$s0, 0($sp)			# done saving registers
+		
+		sll		$s2, $a1, 2			# s2 = 4*N (number of bytes per row)
+		multu	$a2, $s2			# result will be 32-bit unless the matrix is huge
+		mflo	$s1					# s1 = a*s2
+		addu	$s1, $s1, $a0		# Now s1 contains address to row a
+		sll		$s0, $a3, 2			# s0 = 4*b (byte offset of column b)
+		addu	$v0, $s1, $s0		# Now we have address to A[a][b] in v0...
+		l.s		$f0, 0($v0)		    # ... and contents of A[a][b] in f0.
+		
+		lw		$s2, 8($sp)
+		lw		$s1, 4($sp)
+		lw		$s0, 0($sp)			# done restoring registers
+		addiu	$sp, $sp, 12		# remove stack frame
+		
+		jr		$ra					# return from subroutine
+		nop							# this is the delay slot associated with all types of jumps
+
+################################################################################
+# print_matrix
+#
+# This routine is for debugging purposes only. 
+# Do not call this routine when timing your code!
+#
+# print_matrix uses floating point register $f12.
+# the value of $f12 is _not_ preserved across calls.
+#
+# Args:		$a0  - base address of matrix (A)
+#			$a1  - number of elements per row (N) 
+print_matrix:
+		addiu	$sp,  $sp, -20		# allocate stack frame
+		sw		$ra,  16($sp)
+		sw      $s2,  12($sp)
+		sw		$s1,  8($sp)
+		sw		$s0,  4($sp) 
+		sw		$a0,  0($sp)		# done saving registers
+
+		move	$s2,  $a0			# s2 = a0 (array pointer)
+		move	$s1,  $zero			# s1 = 0  (row index)
+loop_s1:
+		move	$s0,  $zero			# s0 = 0  (column index)
+loop_s0:
+		l.s		$f12, 0($s2)        # $f12 = A[s1][s0]
+		li		$v0,  2				# specify print float system call
+ 		syscall						# print A[s1][s0]
+		la		$a0,  spaces
+		li		$v0,  4				# specify print string system call
+		syscall						# print spaces
+
+		addiu	$s2,  $s2, 4		# increment pointer by 4
+
+		addiu	$s0,  $s0, 1        # increment s0
+		blt		$s0,  $a1, loop_s0  # loop while s0 < a1
+		nop
+		la		$a0,  newline
+		syscall						# print newline
+		addiu	$s1,  $s1, 1		# increment s1
+		blt		$s1,  $a1, loop_s1  # loop while s1 < a1
+		nop
+		la		$a0,  newline
+		syscall						# print newline
+
+		lw		$ra,  16($sp)
+		lw		$s2,  12($sp)
+		lw		$s1,  8($sp)
+		lw		$s0,  4($sp)
+		lw		$a0,  0($sp)		# done restoring registers
+		addiu	$sp,  $sp, 20		# remove stack frame
+
+		jr		$ra					# return from subroutine
+		nop							# this is the delay slot associated with all types of jumps
+
+### End of text segment
+
+### Data segment 
+		.data
+		
+### String constants
+spaces:
+		.asciiz "   "   			# spaces to insert between numbers
+newline:
+		.asciiz "\n"  				# newline
+
+### Float constants
+float1.0:
+		.float 1.0
+## Input matrix: (4x4) ##
+matrix_4x4:	
+		.float 57.0
+		.float 20.0
+		.float 34.0
+		.float 59.0
+		
+		.float 104.0
+		.float 19.0
+		.float 77.0
+		.float 25.0
+		
+		.float 55.0
+		.float 14.0
+		.float 10.0
+		.float 43.0
+		
+		.float 31.0
+		.float 41.0
+		.float 108.0
+		.float 59.0
+		
+		# These make it easy to check if 
+		# data outside the matrix is overwritten
+		.word 0xdeadbeef
+		.word 0xdeadbeef
+		.word 0xdeadbeef
+		.word 0xdeadbeef
+		.word 0xdeadbeef
+		.word 0xdeadbeef
+		.word 0xdeadbeef
+		.word 0xdeadbeef
+
+## Input matrix: (24x24) ##
+matrix_24x24:
+		.float	 92.00 
+		.float	 43.00 
+		.float	 86.00 
+		.float	 87.00 
+		.float	100.00 
+		.float	 21.00 
+		.float	 36.00 
+		.float	 84.00 
+		.float	 30.00 
+		.float	 60.00 
+		.float	 52.00 
+		.float	 69.00 
+		.float	 40.00 
+		.float	 56.00 
+		.float	104.00 
+		.float	100.00 
+		.float	 69.00 
+		.float	 78.00 
+		.float	 15.00 
+		.float	 66.00 
+		.float	  1.00 
+		.float	 26.00 
+		.float	 15.00 
+		.float	 88.00 
+
+		.float	 17.00 
+		.float	 44.00 
+		.float	 14.00 
+		.float	 11.00 
+		.float	109.00 
+		.float	 24.00 
+		.float	 56.00 
+		.float	 92.00 
+		.float	 67.00 
+		.float	 32.00 
+		.float	 70.00 
+		.float	 57.00 
+		.float	 54.00 
+		.float	107.00 
+		.float	 32.00 
+		.float	 84.00 
+		.float	 57.00 
+		.float	 84.00 
+		.float	 44.00 
+		.float	 98.00 
+		.float	 31.00 
+		.float	 38.00 
+		.float	 88.00 
+		.float	101.00 
+
+		.float	  7.00 
+		.float	104.00 
+		.float	 57.00 
+		.float	  9.00 
+		.float	 21.00 
+		.float	 72.00 
+		.float	 97.00 
+		.float	 38.00 
+		.float	  7.00 
+		.float	  2.00 
+		.float	 50.00 
+		.float	  6.00 
+		.float	 26.00 
+		.float	106.00 
+		.float	 99.00 
+		.float	 93.00 
+		.float	 29.00 
+		.float	 59.00 
+		.float	 41.00 
+		.float	 83.00 
+		.float	 56.00 
+		.float	 73.00 
+		.float	 58.00 
+		.float	  4.00 
+
+		.float	 48.00 
+		.float	102.00 
+		.float	102.00 
+		.float	 79.00 
+		.float	 31.00 
+		.float	 81.00 
+		.float	 70.00 
+		.float	 38.00 
+		.float	 75.00 
+		.float	 18.00 
+		.float	 48.00 
+		.float	 96.00 
+		.float	 91.00 
+		.float	 36.00 
+		.float	 25.00 
+		.float	 98.00 
+		.float	 38.00 
+		.float	 75.00 
+		.float	105.00 
+		.float	 64.00 
+		.float	 72.00 
+		.float	 94.00 
+		.float	 48.00 
+		.float	101.00 
+
+		.float	 43.00 
+		.float	 89.00 
+		.float	 75.00 
+		.float	100.00 
+		.float	 53.00 
+		.float	 23.00 
+		.float	104.00 
+		.float	101.00 
+		.float	 16.00 
+		.float	 96.00 
+		.float	 70.00 
+		.float	 47.00 
+		.float	 68.00 
+		.float	 30.00 
+		.float	 86.00 
+		.float	 33.00 
+		.float	 49.00 
+		.float	 24.00 
+		.float	 20.00 
+		.float	 30.00 
+		.float	 61.00 
+		.float	 45.00 
+		.float	 18.00 
+		.float	 99.00 
+
+		.float	 11.00 
+		.float	 13.00 
+		.float	 54.00 
+		.float	 83.00 
+		.float	108.00 
+		.float	102.00 
+		.float	 75.00 
+		.float	 42.00 
+		.float	 82.00 
+		.float	 40.00 
+		.float	 32.00 
+		.float	 25.00 
+		.float	 64.00 
+		.float	 26.00 
+		.float	 16.00 
+		.float	 80.00 
+		.float	 13.00 
+		.float	 87.00 
+		.float	 18.00 
+		.float	 81.00 
+		.float	  8.00 
+		.float	104.00 
+		.float	  5.00 
+		.float	 57.00 
+
+		.float	 19.00 
+		.float	 26.00 
+		.float	 87.00 
+		.float	 80.00 
+		.float	 72.00 
+		.float	106.00 
+		.float	 70.00 
+		.float	 83.00 
+		.float	 10.00 
+		.float	 14.00 
+		.float	 57.00 
+		.float	  8.00 
+		.float	  7.00 
+		.float	 22.00 
+		.float	 50.00 
+		.float	 90.00 
+		.float	 63.00 
+		.float	 83.00 
+		.float	  5.00 
+		.float	 17.00 
+		.float	109.00 
+		.float	 22.00 
+		.float	 97.00 
+		.float	 13.00 
+
+		.float	109.00 
+		.float	  5.00 
+		.float	 95.00 
+		.float	  7.00 
+		.float	  0.00 
+		.float	101.00 
+		.float	 65.00 
+		.float	 19.00 
+		.float	 17.00 
+		.float	 43.00 
+		.float	100.00 
+		.float	 90.00 
+		.float	 39.00 
+		.float	 60.00 
+		.float	 63.00 
+		.float	 49.00 
+		.float	 75.00 
+		.float	 10.00 
+		.float	 58.00 
+		.float	 83.00 
+		.float	 33.00 
+		.float	109.00 
+		.float	 63.00 
+		.float	 96.00 
+
+		.float	 82.00 
+		.float	 69.00 
+		.float	  3.00 
+		.float	 82.00 
+		.float	 91.00 
+		.float	101.00 
+		.float	 96.00 
+		.float	 91.00 
+		.float	107.00 
+		.float	 81.00 
+		.float	 99.00 
+		.float	108.00 
+		.float	 73.00 
+		.float	 54.00 
+		.float	 18.00 
+		.float	 91.00 
+		.float	 97.00 
+		.float	  8.00 
+		.float	 71.00 
+		.float	 27.00 
+		.float	 69.00 
+		.float	 25.00 
+		.float	 77.00 
+		.float	 34.00 
+
+		.float	 36.00 
+		.float	 25.00 
+		.float	  8.00 
+		.float	 69.00 
+		.float	 24.00 
+		.float	 71.00 
+		.float	 56.00 
+		.float	106.00 
+		.float	 30.00 
+		.float	 60.00 
+		.float	 79.00 
+		.float	 12.00 
+		.float	 51.00 
+		.float	 65.00 
+		.float	103.00 
+		.float	 49.00 
+		.float	 36.00 
+		.float	 93.00 
+		.float	 47.00 
+		.float	  0.00 
+		.float	 37.00 
+		.float	 65.00 
+		.float	 91.00 
+		.float	 25.00 
+
+		.float	 74.00 
+		.float	 53.00 
+		.float	 53.00 
+		.float	 33.00 
+		.float	 78.00 
+		.float	 20.00 
+		.float	 68.00 
+		.float	  4.00 
+		.float	 45.00 
+		.float	 76.00 
+		.float	 74.00 
+		.float	 70.00 
+		.float	 38.00 
+		.float	 20.00 
+		.float	 67.00 
+		.float	 68.00 
+		.float	 80.00 
+		.float	 36.00 
+		.float	 81.00 
+		.float	 22.00 
+		.float	101.00 
+		.float	 75.00 
+		.float	 71.00 
+		.float	 28.00 
+
+		.float	 58.00 
+		.float	  9.00 
+		.float	 28.00 
+		.float	 96.00 
+		.float	 75.00 
+		.float	 10.00 
+		.float	 12.00 
+		.float	 39.00 
+		.float	 63.00 
+		.float	 65.00 
+		.float	 73.00 
+		.float	 31.00 
+		.float	 85.00 
+		.float	 31.00 
+		.float	 36.00 
+		.float	 20.00 
+		.float	108.00 
+		.float	  0.00 
+		.float	 91.00 
+		.float	 36.00 
+		.float	 20.00 
+		.float	 48.00 
+		.float	105.00 
+		.float	101.00 
+
+		.float	 84.00 
+		.float	 76.00 
+		.float	 13.00 
+		.float	 75.00 
+		.float	 42.00 
+		.float	 85.00 
+		.float	103.00 
+		.float	100.00 
+		.float	 94.00 
+		.float	 22.00 
+		.float	 87.00 
+		.float	 60.00 
+		.float	 32.00 
+		.float	 99.00 
+		.float	100.00 
+		.float	 96.00 
+		.float	 54.00 
+		.float	 63.00 
+		.float	 17.00 
+		.float	 30.00 
+		.float	 95.00 
+		.float	 54.00 
+		.float	 51.00 
+		.float	 93.00 
+
+		.float	 54.00 
+		.float	 32.00 
+		.float	 19.00 
+		.float	 75.00 
+		.float	 80.00 
+		.float	 15.00 
+		.float	 66.00 
+		.float	 54.00 
+		.float	 92.00 
+		.float	 79.00 
+		.float	 19.00 
+		.float	 24.00 
+		.float	 54.00 
+		.float	 13.00 
+		.float	 15.00 
+		.float	 39.00 
+		.float	 35.00 
+		.float	102.00 
+		.float	 99.00 
+		.float	 68.00 
+		.float	 92.00 
+		.float	 89.00 
+		.float	 54.00 
+		.float	 36.00 
+
+		.float	 43.00 
+		.float	 72.00 
+		.float	 66.00 
+		.float	 28.00 
+		.float	 16.00 
+		.float	  7.00 
+		.float	 11.00 
+		.float	 71.00 
+		.float	 39.00 
+		.float	 31.00 
+		.float	 36.00 
+		.float	 10.00 
+		.float	 47.00 
+		.float	102.00 
+		.float	 64.00 
+		.float	 29.00 
+		.float	 72.00 
+		.float	 83.00 
+		.float	 53.00 
+		.float	 17.00 
+		.float	 97.00 
+		.float	 68.00 
+		.float	 56.00 
+		.float	 22.00 
+
+		.float	 61.00 
+		.float	 46.00 
+		.float	 91.00 
+		.float	 43.00 
+		.float	 26.00 
+		.float	 35.00 
+		.float	 80.00 
+		.float	 70.00 
+		.float	108.00 
+		.float	 37.00 
+		.float	 98.00 
+		.float	 14.00 
+		.float	 45.00 
+		.float	  0.00 
+		.float	 86.00 
+		.float	 85.00 
+		.float	 32.00 
+		.float	 12.00 
+		.float	 95.00 
+		.float	 79.00 
+		.float	  5.00 
+		.float	 49.00 
+		.float	108.00 
+		.float	 77.00 
+
+		.float	 23.00 
+		.float	 52.00 
+		.float	 95.00 
+		.float	 10.00 
+		.float	 10.00 
+		.float	 42.00 
+		.float	 33.00 
+		.float	 72.00 
+		.float	 89.00 
+		.float	 14.00 
+		.float	  5.00 
+		.float	  5.00 
+		.float	 50.00 
+		.float	 85.00 
+		.float	 76.00 
+		.float	 48.00 
+		.float	 13.00 
+		.float	 64.00 
+		.float	 63.00 
+		.float	 58.00 
+		.float	 65.00 
+		.float	 39.00 
+		.float	 33.00 
+		.float	 97.00 
+
+		.float	 52.00 
+		.float	 18.00 
+		.float	 67.00 
+		.float	 57.00 
+		.float	 68.00 
+		.float	 65.00 
+		.float	 25.00 
+		.float	 91.00 
+		.float	  7.00 
+		.float	 10.00 
+		.float	101.00 
+		.float	 18.00 
+		.float	 52.00 
+		.float	 24.00 
+		.float	 90.00 
+		.float	 31.00 
+		.float	 39.00 
+		.float	 96.00 
+		.float	 37.00 
+		.float	 89.00 
+		.float	 72.00 
+		.float	  3.00 
+		.float	 28.00 
+		.float	 85.00 
+
+		.float	 68.00 
+		.float	 91.00 
+		.float	 33.00 
+		.float	 24.00 
+		.float	 21.00 
+		.float	 67.00 
+		.float	 12.00 
+		.float	 74.00 
+		.float	 86.00 
+		.float	 79.00 
+		.float	 22.00 
+		.float	 44.00 
+		.float	 34.00 
+		.float	 47.00 
+		.float	 25.00 
+		.float	 42.00 
+		.float	 58.00 
+		.float	 17.00 
+		.float	 61.00 
+		.float	  1.00 
+		.float	 41.00 
+		.float	 42.00 
+		.float	 33.00 
+		.float	 81.00 
+
+		.float	 28.00 
+		.float	 71.00 
+		.float	 60.00 
+		.float	101.00 
+		.float	 75.00 
+		.float	 89.00 
+		.float	 76.00 
+		.float	 34.00 
+		.float	 71.00 
+		.float	  0.00 
+		.float	 58.00 
+		.float	 92.00 
+		.float	 68.00 
+		.float	 70.00 
+		.float	 57.00 
+		.float	 44.00 
+		.float	 39.00 
+		.float	 79.00 
+		.float	 88.00 
+		.float	 74.00 
+		.float	 16.00 
+		.float	  3.00 
+		.float	  6.00 
+		.float	 75.00 
+
+		.float	 20.00 
+		.float	 68.00 
+		.float	 77.00 
+		.float	 62.00 
+		.float	  0.00 
+		.float	  0.00 
+		.float	 33.00 
+		.float	 28.00 
+		.float	 72.00 
+		.float	 94.00 
+		.float	 19.00 
+		.float	 37.00 
+		.float	 73.00 
+		.float	 96.00 
+		.float	 71.00 
+		.float	 34.00 
+		.float	 97.00 
+		.float	 20.00 
+		.float	 17.00 
+		.float	 55.00 
+		.float	 91.00 
+		.float	 74.00 
+		.float	 99.00 
+		.float	 21.00 
+
+		.float	 43.00 
+		.float	 77.00 
+		.float	 95.00 
+		.float	 60.00 
+		.float	 81.00 
+		.float	102.00 
+		.float	 25.00 
+		.float	101.00 
+		.float	 60.00 
+		.float	102.00 
+		.float	 54.00 
+		.float	 60.00 
+		.float	103.00 
+		.float	 87.00 
+		.float	 89.00 
+		.float	 65.00 
+		.float	 72.00 
+		.float	109.00 
+		.float	102.00 
+		.float	 35.00 
+		.float	 96.00 
+		.float	 64.00 
+		.float	 70.00 
+		.float	 83.00 
+
+		.float	 85.00 
+		.float	 87.00 
+		.float	 28.00 
+		.float	 66.00 
+		.float	 51.00 
+		.float	 18.00 
+		.float	 87.00 
+		.float	 95.00 
+		.float	 96.00 
+		.float	 73.00 
+		.float	 45.00 
+		.float	 67.00 
+		.float	 65.00 
+		.float	 71.00 
+		.float	 59.00 
+		.float	 16.00 
+		.float	 63.00 
+		.float	  3.00 
+		.float	 77.00 
+		.float	 56.00 
+		.float	 91.00 
+		.float	 56.00 
+		.float	 12.00 
+		.float	 53.00 
+
+		.float	 56.00 
+		.float	  5.00 
+		.float	 89.00 
+		.float	 42.00 
+		.float	 70.00 
+		.float	 49.00 
+		.float	 15.00 
+		.float	 45.00 
+		.float	 27.00 
+		.float	 44.00 
+		.float	  1.00 
+		.float	 78.00 
+		.float	 63.00 
+		.float	 89.00 
+		.float	 64.00 
+		.float	 49.00 
+		.float	 52.00 
+		.float	109.00 
+		.float	  6.00 
+		.float	  8.00 
+		.float	 70.00 
+		.float	 65.00 
+		.float	 24.00 
+		.float	 24.00 
+
+### End of data segment
